81079d26daa892100fff826c7ee477bdb1e96613
Muhammad Afifudin <cipoghost@gmail.com>
2020-10-27 13:29:32 +0700

    add helper to convert date easily

---
+++ application/helpers/date_convert.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author   Natan Felles <natanfelles@gmail.com>
+ */
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+if ( ! function_exists('simple_date')) {
+    function simple_date($date) {
+        return date('Y-m-d', $date);
+    }
+}
