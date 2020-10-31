3f76a37b26ad3062523d468f862fd04684f22c85
hermawanln <hermawan.luthfi@gmail.com>
2020-10-31 18:37:27 +0700

    add login view

---
+++ application/views/admin/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta charset="utf-8" />
+        <title><?php echo $title; ?></title>
+
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+        <!-- bootstrap & fontawesome -->
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/font-awesome/4.5.0/css/font-awesome.min.css'); ?>" />
+
+        <!-- text fonts -->
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/fonts.googleapis.com.css'); ?>" />
+
+        <!-- ace styles -->
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/ace.min.css'); ?>" class="ace-main-stylesheet" id="main-ace-style" />
+
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/ace-skins.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/ace-rtl.min.css'); ?>" />
+        <script src="<?php echo base_url('assets/js/ace-extra.min.js'); ?>"></script>
+    </head>
